2ade582dbd69f98a2c6df966d71ace3d35e139df
Noobul <ionoobmaster@hotmail.com>
2021-03-22 12:50:46 +0200

    ch8 p4

---
+++ Ch8/8-12 Sandwiches.py
@@ -0,0 +1,6 @@
+def sandwich_items(*items):
+    print(f"Your sandwich will contains {items}")
+
+sandwich_items("tomatoes", "salad", "bacon")
+sandwich_items("salami")
+sandwich_items("cheese", "more cheese", "even more cheese")
---
+++ Ch8/8-14 Cars.py
@@ -0,0 +1,8 @@
+def make_car(manufacturer, model, **car_details):
+    """Create car details"""
+    car_details["manufacturer_name"] = manufacturer.title()
+    car_details["model_name"] = model.title()
+    return car_details
+
+car = make_car('subaru', 'outback', color = 'blue', tow_package = True)
+print(car)
